655b4dd6b2ec72c3bf360e8504fd18cc5a1a39bf
nishra <nishrasshah@gmail.com>
2025-02-12 00:03:23 -0800

    file age added to remote repository

---
+++ age.php
@@ -0,0 +1,27 @@
+<?php
+     print_r(value: $_POST);
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form validation</title>
+</head>
+<body>
+    <h1>This is my Form</h1>
+    <form action="" method="POST">
+        age:<input type="number" name="age" value="">
+        submit:<button>submit</button>
+    </form>
+
+</body>
+</html>
+
